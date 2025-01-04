b79cc48379feb7a06f47e0d9a131bea5ba47c7b3
songalini <songalinidev@gmail.com>
2025-01-04 12:36:38 +1100

    B: drawing player to screen

--- main.py
+++ main.py
@@ -1,17 +1,33 @@
 import pygame
 from constants import *
+from circleshape import *
+from player import *
 
 def main():
     pygame.init
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH/ 2, SCREEN_HEIGHT /2)
     while True:
-        screen.fill
-        pygame.display.flip()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-              return
+                return
+
+
+
+
+        dt = clock.tick(60) / 1000
+        player.update(dt)
+        screen.fill("black")
+        player.draw(screen)
+        pygame.display.flip()
+
+
+
+
 if __name__ == "__main__":
     main()
